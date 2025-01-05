7b9cac5042747d3b510f52d8fe1ebc8724f2e00c
Weiam1 <145970015+Weiam1@users.noreply.github.com>
2025-01-05 18:26:42 +0100

    Sections

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
 </div>
 
 
-<!-- RECIPE CARD -->
+<!-- Post section -->
 <div class="container py-5">
    <div class="row align-items-center">
       <!-- Image Section -->
@@ -29,6 +29,56 @@
          </div>
       </div>
    </div>
+
+
+ <!-- Blog Categories -->
+<div class="blog-categories">
+   <h2 class="blog-title">Blog Categories</h2>
+   <div class="container">
+      <div class="row row-cols-2 row-cols-md-4 g-3">
+         <div class="col">
+            <a href="#lunch" class="blog-category">Lunch</a>
+         </div>
+         <div class="col">
+            <a href="#healthy" class="blog-category">Healthy</a>
+         </div>
+         <div class="col">
+            <a href="#dessert" class="blog-category">Dessert</a>
+         </div>
+         <div class="col">
+            <a href="#drinks" class="blog-category">Drinks</a>
+         </div>
+      </div>
+   </div>
+</div>
+
+
+<!-- Sections -->
+<!-- Lunch Section -->
+<div id="lunch" class="category-section">
+   <h3 class="category-title">Lunch Recipes</h3>
+   <p>Here are some delicious lunch recipes...</p>
+</div>
+
+<!-- Healthy Section -->
+<div id="healthy" class="category-section">
+   <h3 class="category-title">Healthy Recipes</h3>
+   <p>Here are some healthy recipes for a better lifestyle...</p>
+</div>
+
+<!-- Dessert Section -->
+<div id="dessert" class="category-section">
+   <h3 class="category-title">Dessert Recipes</h3>
+   <p>Indulge in our selection of sweet dessert recipes...</p>
+</div>
+
+<!-- Drinks Section -->
+<div id="drinks" class="category-section">
+   <h3 class="category-title">Drinks Recipes</h3>
+   <p>Cool down or warm up with these drink recipes...</p>
+</div>
+
+
 
 
 
